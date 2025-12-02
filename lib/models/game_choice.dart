95be28d9382bb7d4d11e7c2b2ec30d79e78af4f6
rmuthuswamy21 <80219766+rmuthuswamy21@users.noreply.github.com>
2025-12-02 18:49:25 +0000

    adding new options for gamechoice

--- lib/models/game_choice.dart
+++ lib/models/game_choice.dart
@@ -1,6 +1,20 @@
 class GameChoice {
   final String text;
   final String nextId;
+  final int ph;
+  final int mh;
+  final int tc;
+  final int ss;
+  final int cr; 
 
-  const GameChoice({required this.text, required this.nextId});
+
+  const GameChoice({
+    required this.text,
+    required this.nextId,
+    required this.ph,
+    required this.mh,
+    required this.tc,
+    required this.ss,
+    required this.cr,
+  });
 }
